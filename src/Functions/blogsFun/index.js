4c1c1f5177475c36a9e4b50ddc41ad9a088e94f4
garvmehta <garvmehta7701@gmail.com>
2023-03-24 11:58:32 +0530

    edit blog delete blog add comment in fun created

--- src/Functions/blogsFun/index.js
+++ src/Functions/blogsFun/index.js
@@ -26,3 +26,85 @@ export const getAllBlogFun = async ()=>{
     }
     
 }
+
+
+export const editBlogFun = async ({blogId, toUpdate })=>{
+    try{
+        
+    
+         const res =  await baseUrl.put(`/blogs/update/${blogId}`,toUpdate);
+         console.log(res.data);
+         if(res.data.status){
+             return {
+                status: true,
+                response:res.data.data,
+             }
+
+         }else{
+            return {
+                status: false,
+                errorMessage:res.data.message,
+             }
+         }
+    }catch(catchError){
+        return {
+            status:false,
+            errorMessage: catchError
+        }
+    }
+    
+}
+
+export const deleteBlogFun = async ({blogId})=>{
+    try{
+        
+    
+         const res =  await baseUrl.delete(`/blogs/delete/${blogId}`);
+         console.log(res.data);
+         if(res.data.status){
+             return {
+                status: true,
+                response:res.data.data,
+             }
+
+         }else{
+            return {
+                status: false,
+                errorMessage:res.data.message,
+             }
+         }
+    }catch(catchError){
+        return {
+            status:false,
+            errorMessage: catchError
+        }
+    }
+    
+}
+
+export const addCommentFun = async ({blogId, commentData})=>{
+    try{
+        
+    
+         const res =  await baseUrl.post(`/blogs/comments/${blogId}`, commentData);
+         console.log(res.data);
+         if(res.data.status){
+             return {
+                status: true,
+                response:res.data.data,
+             }
+
+         }else{
+            return {
+                status: false,
+                errorMessage:res.data.message,
+             }
+         }
+    }catch(catchError){
+        return {
+            status:false,
+            errorMessage: catchError
+        }
+    }
+    
+}
--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -2,44 +2,79 @@ import { Box, Flex, Heading, VStack, Text, Button } from "@chakra-ui/react";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import TextInput from "../../Components/TextInput";
-import { getAllBlogFun } from "../../Functions/blogsFun";
-import { deleteuserFun, forgetPasswordFun, getAllUserFun, loginFun, signupFun } from "../../Functions/userFun";
+import { deleteBlogFun, editBlogFun, getAllBlogFun } from "../../Functions/blogsFun";
+import {
+  deleteuserFun,
+  forgetPasswordFun,
+  getAllUserFun,
+  loginFun,
+  signupFun,
+} from "../../Functions/userFun";
 const Login = () => {
-    useEffect(()=>{
-        // loginFun({email:'nadeem@gmail.com',password:'123456'});
-        // signupFun({name:'garv',email:'garvmehta@gmail.com',password:'1234567', type:'admin'})
-        // forgetPasswordFun({email:'garvmehta@gmail.com', password:'123456789'});
-        // deleteuserFun({userId:"641a8dec362aa39fdd018f96"});
-        // getAllUserFun();
-        getAllBlogFun();
-    },[])
-    return (<>
-        <Flex bg={'blue.200'} align={'center'} justify={'center'} w='100%' h={'100vh'} >
-            <VStack justify={'center'} width={'30%'} bg={'white'} p={8} rounded={'lg'} boxShadow={'md'}>
-                <Flex direction={'column'} align={'center'}>
-
-                    <Heading size={'md'} fontWeight={'medium'} >Login</Heading>
-                    <Text color={'gray.400'} fontSize={'sm'} py={'1'} >Please Login To Portal</Text>
-                </Flex>
-                <TextInput
-                    title={'Email'}
-                    placeholder={'Enter Email'}
-                    type={'email'}
-                />
-                <TextInput
-                    title={'Password'}
-                    placeholder={'Enter Password'}
-                    type={'password'}
-                />
-                <Box py={'1'} w={'100%'}>
-                    <Button bg={'blue.300'} w={'100%'} color={'white'}  >Login</Button>
-                </Box>
-                <Link to={'/forget'}>
-                <Button variant={'link'} fontWeight={'normal'} fontSize={'sm'} >Forget Password</Button>
-                </Link>
-            </VStack>
-        </Flex>
-    </>)
-
-}
-export default Login
+  useEffect(() => {
+    // loginFun({email:'nadeem@gmail.com',password:'123456'});
+    // signupFun({name:'garv',email:'garvmehta@gmail.com',password:'1234567', type:'admin'})
+    // forgetPasswordFun({email:'garvmehta@gmail.com', password:'123456789'});
+    // deleteuserFun({userId:"641a8dec362aa39fdd018f96"});
+    // getAllUserFun();
+    // getAllBlogFun();
+    // editBlogFun({
+    //   blogId: "64155b6df5675b35526a26d9",
+    //   toUpdate: {
+    //     title: "letest Blog ",
+    //     description: "new blog commming soon",
+    //   },
+    // });
+    // deleteBlogFun({blogId:'64155b6df5675b35526a26d9'});
+  }, []);
+  return (
+    <>
+      <Flex
+        bg={"blue.200"}
+        align={"center"}
+        justify={"center"}
+        w="100%"
+        h={"100vh"}
+      >
+        <VStack
+          justify={"center"}
+          width={"30%"}
+          bg={"white"}
+          p={8}
+          rounded={"lg"}
+          boxShadow={"md"}
+        >
+          <Flex direction={"column"} align={"center"}>
+            <Heading size={"md"} fontWeight={"medium"}>
+              Login
+            </Heading>
+            <Text color={"gray.400"} fontSize={"sm"} py={"1"}>
+              Please Login To Portal
+            </Text>
+          </Flex>
+          <TextInput
+            title={"Email"}
+            placeholder={"Enter Email"}
+            type={"email"}
+          />
+          <TextInput
+            title={"Password"}
+            placeholder={"Enter Password"}
+            type={"password"}
+          />
+          <Box py={"1"} w={"100%"}>
+            <Button bg={"blue.300"} w={"100%"} color={"white"}>
+              Login
+            </Button>
+          </Box>
+          <Link to={"/forget"}>
+            <Button variant={"link"} fontWeight={"normal"} fontSize={"sm"}>
+              Forget Password
+            </Button>
+          </Link>
+        </VStack>
+      </Flex>
+    </>
+  );
+};
+export default Login;
